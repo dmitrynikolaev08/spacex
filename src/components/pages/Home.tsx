b9948d68f1662dd179b042134b62de640b410131
Dmitrii Nikolaev <nikd01@vse.cz>
2022-02-01 11:59:40 +0400

    add load more button

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,10 +1,63 @@
-import React from 'react'
-import { Launches } from '../../context/Launches';
+import React, { useState, useEffect } from 'react'
+import { Launch, Launches, LaunchesData } from '../../context/Launches';
+import { gql } from '@apollo/client'
+import { client } from '../../common/apollo-client'
+
+const pageSize = 10;
+let arrayForHoldingLaunches: Launch[] = [];
+const GET_LAUNCHES = gql`
+    query GetLaunches($limit: Int, $offset: Int) {
+            launches (limit: $limit, offset: $offset) {
+            id
+            mission_name
+            launch_date_local
+            rocket {
+                rocket_name
+            }
+        }
+    }
+     
+`;
 
 const Home: React.FC = () => {
+    const [launchesToShow, setLaunchesToShow] = useState<Launch[]>([]);
+    const [next, setNext] = useState(pageSize);
+
+    const loadLaunches = async (limit: number, offset: number) => {
+        let result;
+        await client.query<LaunchesData>(
+            {
+                query: GET_LAUNCHES,
+                variables: { limit: limit, offset: offset }
+            }).then((response) => {
+                console.log(response.data)
+                result = response.data
+            });
+        return result !== undefined ? (result as LaunchesData).launches : [];
+    }
+
+    const loopWithSlice = async (start: number, end: number) => {
+        const newLaunches = await loadLaunches(start, end);
+        if (newLaunches !== undefined) {
+            arrayForHoldingLaunches.push(...newLaunches);
+            setLaunchesToShow(arrayForHoldingLaunches);
+        }
+
+    };
+
+    useEffect(() => {
+        loopWithSlice(0, pageSize);
+    }, []);
+
+    const handleShowMoreLaunches = () => {
+        loopWithSlice(next, next + pageSize);
+        setNext(next + pageSize);
+    };
+
     return (
         <div>
-            <Launches />
+            <Launches launches={launchesToShow} />
+            <button onClick={handleShowMoreLaunches}>Load more</button>
         </div>
     );
 }
--- src/context/Launches.tsx
+++ src/context/Launches.tsx
@@ -1,66 +1,94 @@
-import React, { Fragment } from 'react'
-import { Button } from 'react-bootstrap'
-import { useQuery, gql } from '@apollo/client'
+import React, { ComponentProps, Fragment } from 'react'
+// import { Button } from 'react-bootstrap'
+// import { useQuery, gql } from '@apollo/client'
 
-interface Launch {
+export interface Launch {
     id: string;
     mission_name: string;
-    launch_date_local: string
+    launch_date_local: string;
+    rocket_name: string;
 }
 
-interface LaunchesData {
+export interface LaunchesData {
     launches: Launch[]
 }
 
 
+// interface ILanchesRequest {
+//     limit?: number
+//     offset?: number
+// }
+
+// let loadedLaunches: Array<Launch> = [];
+
+// export const Launches: React.FC<ILanchesRequest> = ({ limit, offset }) => {
+//     const { loading, data } = useQuery<LaunchesData>(gql`
+//         query {
+//                 launches (limit: ${limit}, offset: ${offset}) {
+//                 id
+//                 mission_name
+//                 launch_date_local
+//                 rocket {
+//                     rocket_name
+//                 }
+//             }
+//         } 
+//     `);
+
+//     if (data !== undefined) {
+//         loadedLaunches.push(...data.launches);
+//     }
+
+//     return (
+//         <div>
+//             <h3>Most recent launches</h3>
+//             {loading ? (<p>Loading...</p>
+//             ) : (
+//                 <Fragment>
+//                     {loadedLaunches.map(launch => (
+//                         <div>
+//                             <h5>{launch.id}</h5>
+//                             <p>{launch.mission_name}</p>
+//                             <p>{launch.launch_date_local}</p>
+//                         </div>
+//                     ))}
+//                     <Button variant="outline-info" onClick={() => {
+
+//                     }}>Load more...</Button>{' '}
+//                 </Fragment>
+//             )}
+//         </div>
+//     )
+// }
+
+// Launches.defaultProps = {
+//     limit: 10,
+//     offset: 0
+// }
+
+
 interface ILanchesRequest {
-    limit?: number
-    offset?: number
+    loading?: boolean
+    launches: Launch[]
 }
 
-let loadedLaunches: Array<Launch> = [];
-
-export const Launches: React.FC<ILanchesRequest> = ({ limit, offset }) => {
-    const { loading, data } = useQuery<LaunchesData>(gql`
-        query {
-                launches (limit: ${limit}, offset: ${offset}) {
-                id
-                mission_name
-                launch_date_local
-                rocket {
-                    rocket_name
-                }
-            }
-        } 
-    `);
-
-    if (data !== undefined) {
-        loadedLaunches.push(...data.launches);
-    }
-
+export const Launches: React.FC<ILanchesRequest> = ({ loading, launches }) => {
     return (
         <div>
             <h3>Most recent launches</h3>
             {loading ? (<p>Loading...</p>
             ) : (
                 <Fragment>
-                    {loadedLaunches.map(launch => (
+                    {launches.map(launch => (
                         <div>
                             <h5>{launch.id}</h5>
                             <p>{launch.mission_name}</p>
                             <p>{launch.launch_date_local}</p>
                         </div>
                     ))}
-                    <Button variant="outline-info" onClick={() => {
-
-                    }}>Load more...</Button>{' '}
                 </Fragment>
             )}
         </div>
     )
 }
 
-Launches.defaultProps = {
-    limit: 10,
-    offset: 0
-}
